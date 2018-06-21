82227c4f00f16ca1f5636e84217f69764284bc70
lakhansamani <lakhan.m.samani@gmail.com>
2018-06-21 22:40:34 +0530

    Add todo component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from "@angular/platform-browser";
+import { TodoComponent } from './todo.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule
   ],
-  declarations: []
+  declarations: [TodoComponent],
+  exports: [TodoComponent]
 })
 export class TodoModule { }
